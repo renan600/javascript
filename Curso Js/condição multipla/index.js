bcfdd3ad05a097823ed80a228cd7caaca985b846
renan600 <ragazonrenan@gmail.com>
2021-11-09 17:52:29 -0300

    atualização

--- Curso Js/condição multipla/index.js
+++
@@ -1,2 +0,0 @@
-var idade = 18 
-
--- Curso Js/condição simples e composta/condicao.js
+++
@@ -1,8 +0,0 @@
-var pais = ''
-console.log(`Você é do ${pais}`)
-
-if( pais == 'Brasil'){
-    console.log('Você é Brasileiro')
-}else{
-    console.log('Você não é Brasileiro')
-}
